0f82c80143aa0df9102f41cbc0b8c7fe3c074e60
Eero Heikkinen <eero@kamupak.com>
2022-02-12 18:11:12 +0200

    Persist twitter credentials after auth

--- src/auth/twitter.strategy.ts
+++ src/auth/twitter.strategy.ts
@@ -4,12 +4,15 @@ import { Profile, Strategy } from 'passport-twitter';
 
 import * as dotenv from "dotenv";
 import { User } from 'src/user/entities/user.entity';
+import { SocialCredentialService } from 'src/social-credential/social-credential.service';
+import { UserService } from 'src/user/user.service';
 
 dotenv.config();
 
 @Injectable()
 export class TwitterStrategy extends PassportStrategy(Strategy, 'twitter') {
-  constructor() {
+  constructor(private socialCredentialService: SocialCredentialService,
+    private userService: UserService) {
     super({
       consumerKey: process.env.TWITTER_KEY,
       consumerSecret: process.env.TWITTER_SECRET,
@@ -23,12 +26,54 @@ export class TwitterStrategy extends PassportStrategy(Strategy, 'twitter') {
     profile: any,
     done: (err: any, user: any, info?: any) => void,
   ): Promise<any> {
+    const { username, id, displayName } = profile;
+
+    let firstName;
+    if(displayName) {
+      firstName = displayName.split(" ")[0]
+    }
+
+    let credentials = await this.socialCredentialService.findByProfileIdAndPlatform(id, 'twitter');
     
+    let userid: string;
+
+    if(!credentials.length) {
+      // First login for user
+      const Uuid = require('cassandra-driver').types.Uuid;
+      userid = Uuid.random().toString();
+
+      const newUser = await this.userService.create({
+        userid,
+        firstName
+      });
+
+      credentials = await this.socialCredentialService.create({
+        userid,
+        profile_id: id,
+        platform: 'twitter',
+        auth_token: token,
+        token_secret: tokenSecret,
+        auth_expiration: undefined
+      })
+    } else {
+      userid = credentials[0].userid;
+      
+      // Update new access token (to all tables)
+      credentials = await this.socialCredentialService.update({
+        userid,
+        profile_id: id,
+        platform: 'twitter',
+        auth_token: token,
+        token_secret: tokenSecret,
+        auth_expiration: undefined
+      })
+    }
+
     const user:User = {
-      userid: 'unimplemented',
-      firstName: 'unimplemented'
+      userid,
+      firstName: firstName
     };
 
-    done(null, user);
+    done(null, user);  
   }
 }
