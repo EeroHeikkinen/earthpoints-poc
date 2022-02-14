7248ec85ff8d50e566ef11c3504cc50d9c0e2701
Eero Heikkinen <eero@kamupak.com>
2022-02-14 19:05:58 +0200

    Remove unnecessary dependency

--- src/adapter/impl/twitter.adapter.ts
+++ src/adapter/impl/twitter.adapter.ts
@@ -1,7 +1,6 @@
 import { TwitterApi } from 'twitter-api-v2';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { emit } from 'process';
-import { FacebookService } from 'src/facebook/facebook.service';
 import { RuleService } from 'src/rule/rule.service';
 import { IPlatformAdapter } from 'src/interfaces/platform-adapter.interface';
 import { SocialCredential } from 'src/social-credential/entities/social-credential.entity';
@@ -13,7 +12,6 @@ dotenv.config();
 @Injectable()
 export class TwitterAdapter implements IPlatformAdapter {
     constructor(
-        private facebookService: FacebookService,
         private ruleService: RuleService
         ){}
 
