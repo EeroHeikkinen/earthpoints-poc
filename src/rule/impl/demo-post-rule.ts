f18eaefc05107aa18041120eb73ffd21dac223b0
Eero Heikkinen <eero@kamupak.com>
2022-03-03 21:06:15 +0200

    retrieve image urls if available

--- src/rule/impl/demo-post-rule.ts
+++ src/rule/impl/demo-post-rule.ts
@@ -8,34 +8,46 @@ import { IPointRule } from 'src/interfaces/point-rule.interface';
 import { stringify } from 'querystring';
 import { PointEvent } from 'src/point-event/entities/point-event.entity';
 
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 import { CreatePointEventDto } from 'src/point-event/dto/create-point-event.dto';
 dotenv.config();
 
 @Injectable()
 export class DemoPostRule implements IPointRule {
-    private hashkey = 'demopostrule';
+  private hashkey = 'demopostrule';
 
-    async process(eventName: any, item:any): Promise<false | CreatePointEventDto> {
-        if(eventName.startsWith('user.published.post')) {
-            if(item.message && item.message.toLowerCase().includes("#" + process.env.REWARD_HASHTAG.toLowerCase())) {
-                var hashString = item.platform + item.data.id + this.hashkey;
-                
-                const pointEvent:CreatePointEventDto = {
-                    hashString: hashString,
-                    userid: item.userid,
-                    isBurn: false,
-                    icon: 'note-sticky',
-                    verb: 'posted on',
-                    platform: item.platform,
-                    points: parseInt(process.env.REWARD_POINTS),
-                    timestamp: item.data.timestamp,
-                    message: process.env.REWARD_MESSAGE,
-                    metadata: new Map<string, string>()
-                }
-                return pointEvent;
-            }
+  async process(
+    eventName: any,
+    item: any,
+  ): Promise<false | CreatePointEventDto> {
+    if (eventName.startsWith('user.published.post')) {
+      if (
+        item.message &&
+        item.message
+          .toLowerCase()
+          .includes('#' + process.env.REWARD_HASHTAG.toLowerCase())
+      ) {
+        const hashString = item.platform + item.data.id + this.hashkey;
+
+        let icon = 'note-sticky';
+        if (item.imageDownloader) {
+          icon = await item.imageDownloader();
         }
-        return false;
+        const pointEvent: CreatePointEventDto = {
+          hashString: hashString,
+          userid: item.userid,
+          isBurn: false,
+          icon: icon,
+          verb: 'posted on',
+          platform: item.platform,
+          points: parseInt(process.env.REWARD_POINTS),
+          timestamp: item.data.timestamp,
+          message: process.env.REWARD_MESSAGE,
+          metadata: new Map<string, string>(),
+        };
+        return pointEvent;
+      }
     }
+    return false;
+  }
 }
