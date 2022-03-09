cc6ce09c9739f0c72105a42ba718cda77e9c5b2f
Eero Heikkinen <eero@kamupak.com>
2022-03-09 16:50:46 +0200

    Fix imports of test file

--- src/email-template/email-template.controller.spec.ts
+++ src/email-template/email-template.controller.spec.ts
@@ -1,12 +1,36 @@
+import { MailerModule } from '@nestjs-modules/mailer';
 import { Test, TestingModule } from '@nestjs/testing';
+import { CassandraModule } from 'src/cassandra/cassandra.module';
+import { PointEventModule } from 'src/point-event/point-event.module';
+import { EmailContentTemplateRepository } from './email-content-template.repository';
 import { EmailTemplateController } from './email-template.controller';
+import { EmailTemplateModule } from './email-template.module';
+import { EmailTemplateService } from './email-template.service';
+import { SentEmailRepository } from './sent-email.repository';
+import { DailyMessageEmailTemplate } from './templates/daily-message.template';
+import { FiftyPointsEmailTemplate } from './templates/fifty-points-message.template';
+import { WelcomeMessageEmailTemplate } from './templates/welcome-message.template';
 
 describe('EmailTemplateController', () => {
   let controller: EmailTemplateController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        EmailContentTemplateRepository,
+        EmailTemplateService,
+        SentEmailRepository,
+        DailyMessageEmailTemplate,
+        FiftyPointsEmailTemplate,
+        WelcomeMessageEmailTemplate,
+      ],
       controllers: [EmailTemplateController],
+      imports: [
+        PointEventModule,
+        CassandraModule,
+        MailerModule,
+        EmailTemplateModule,
+      ],
     }).compile();
 
     controller = module.get<EmailTemplateController>(EmailTemplateController);
