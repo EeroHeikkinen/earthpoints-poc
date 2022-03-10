bb65fea229ea0af3962c2033bb3b786345b8841c
Eero Heikkinen <eero@kamupak.com>
2022-03-10 15:34:58 +0200

    Change JWT login method to include option for 24h token. Side effect: normal JWT tokens are extended to 1 hour from previous 0.5 hours

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,39 @@
-import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService, JwtSignOptions } from '@nestjs/jwt';
 import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private jwtService: JwtService
-  ) {}
+  constructor(private jwtService: JwtService) {}
 
-  async login(user: User): Promise<any> {
+  validateClientCredentials(clientID: string, clientSecret: string): boolean {
+    if (
+      clientID !== process.env.EXTERNAL_CLIENT_ID &&
+      clientSecret !== process.env.EXTERNAL_CLIENT_SECRET
+    ) {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  async login(user: User, type: 'user' | 'api' = 'user'): Promise<any> {
     const payload = { userid: user.userid };
-    return {
-      access_token: this.jwtService.sign(payload),
+    // For api access token, extend expiry to 24 hours
+    const options: JwtSignOptions = {
+      expiresIn: type === 'api' ? 86400 : 3600,
     };
+    return {
+      access_token: this.jwtService.sign(payload, options),
+      expiresIn: options.expiresIn,
+    };
+  }
+
+  isUserAdmin(user: User) {
+    if (user.userid !== '00000000-0000-0000-0000-000000000000') {
+      return false;
+    }
+    return true;
   }
 }
