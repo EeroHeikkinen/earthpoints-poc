169f74c1ed1afb371c190f948c9f7ab859206752
Eero Heikkinen <eero@kamupak.com>
2022-03-11 17:40:08 +0200

    Add documentation to point event schema

--- src/point-event/dto/create-point-event.dto.ts
+++ src/point-event/dto/create-point-event.dto.ts
@@ -1,55 +1,107 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
-    IsBoolean,
+  IsBoolean,
   IsDateString,
   IsEmail,
-  IsHash,
   IsNotEmpty,
   IsNumber,
   IsOptional,
   IsString,
   IsUUID,
-  Max,
   Min,
-  ValidateIf,
 } from 'class-validator';
 
 export class CreatePointEventDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    description:
+      'The hash string is used to make sure each event will only result in points awarded once. It should be unique for each event and user, in such a way the same hashString will predictably get generated if the same event was sent again. For example, it could be the string "created-pledge-page" followed by the user\'s id in another system.',
+    example: 'created-pledge-page-1234567',
+  })
   hashString?: string;
 
   hash?: string;
 
   @IsOptional()
+  @ApiProperty({
+    required: false,
+    example: '',
+    description:
+      'Already existing User UUID in the system. One of this or an email is required.',
+  })
   @IsUUID()
   userid?: string;
 
   @IsOptional()
+  @ApiProperty({
+    required: false,
+    description:
+      'Email to associate the points with. If an user with this email is not found, a new user will be created. One of this or an User UUID is required.',
+    example: 'my.user@gmail.com',
+  })
   @IsEmail()
   email?: string;
 
-  @IsNotEmpty()
+  @ApiProperty({
+    required: false,
+    description:
+      'Icon to display in web interface (Font Awesome version 6.0.0)',
+    example: 'star',
+  })
   icon?: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    description:
+      'The verb to display along with the point event in user interfaces. For example: "You connected Facebook"',
+    example: 'created a',
+  })
   verb: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    description:
+      'Social platform (twitter, instagram, facebook) or other external platform where the event was awarded from',
+    example: 'pledge-page',
+  })
   platform: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'Message to display to the user.',
+    example: 'You created a pledge page.',
+  })
   message: string;
 
   @IsBoolean()
+  @ApiProperty({
+    description:
+      'For awarding points, set this to false. Set to true if you want to consume points instead of awarding them',
+    example: false,
+  })
   isBurn: boolean;
 
   @IsNotEmpty()
   @IsNumber()
   @Min(0)
+  @ApiProperty({
+    description: 'Number of points to award for this event.',
+    example: 5,
+  })
   points: number;
 
   @IsDateString()
+  @ApiProperty({
+    description:
+      'For displaying to the user, the related time when the points were awarded.',
+    example: '2022-03-11T14:20:20.546Z',
+  })
   timestamp: Date;
 
+  @ApiProperty({
+    type: 'object',
+    description: 'Any custom metadata',
+  })
   metadata: Map<string, string>;
 }
