480f2974fb0e91ce645b17185394f1c1f78ef8a1
Hakan Onal <hakan.onal@yahoo.com>
2022-03-15 10:35:19 +0300

    chore: changed daily message sent time to 20

--- src/email-template/templates/daily-message.template.ts
+++ src/email-template/templates/daily-message.template.ts
@@ -60,7 +60,7 @@ export class DailyMessageEmailTemplate implements IEmailTemplate {
             // Percentile of people
         }*/
 
-        if(hourInUserTimeZone != 12
+        if(hourInUserTimeZone != 20
              ) {
             console.log("Wrong local time to send daily msg")
             return false;
