cf4b12c50dbe18fd3ac6c9fddc8c2b7ef778f856
Hakan Onal <hakan.onal@yahoo.com>
2022-03-16 14:41:40 +0300

    chore: fix test run module def

--- src/point-event/point-event.controller.spec.ts
+++ src/point-event/point-event.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CassandraModule } from 'src/cassandra/cassandra.module';
+import { KafkaModule } from 'src/kafka/kafka.module';
 import { PointEventController } from './point-event.controller';
 import { PointEventRepository } from './point-event.repository';
 import { PointEventService } from './point-event.service';
@@ -11,7 +12,7 @@ describe('PointEventController', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [PointEventService, PointEventRepository],
       controllers: [PointEventController],
-      imports: [CassandraModule],
+      imports: [CassandraModule,KafkaModule],
     }).compile();
 
     controller = module.get<PointEventController>(PointEventController);
