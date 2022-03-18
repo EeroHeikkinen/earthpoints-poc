e17284dcab1d3ca5fc7e65a934011979109673c0
Hakan Onal <hakan.onal@yahoo.com>
2022-03-18 19:14:55 +0300

    chore: fix test result error

--- src/point-event/point-event.service.spec.ts
+++ src/point-event/point-event.service.spec.ts
@@ -1,3 +1,4 @@
+import { BullModule } from '@nestjs/bull';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CassandraModule } from 'src/cassandra/cassandra.module';
 import { PointEventRepository } from './point-event.repository';
@@ -9,7 +10,12 @@ describe('PointEventService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [PointEventService, PointEventRepository],
-      imports: [CassandraModule]
+      imports: [
+        CassandraModule,
+        BullModule.registerQueue({
+        name: 'sse',
+      }),
+    ]
     }).compile();
 
     service = module.get<PointEventService>(PointEventService);
