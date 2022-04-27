f012c54f6f7269bf547b66f89fd0dfc1e6beceb8
Eero Heikkinen <eero@kamupak.com>
2022-04-27 21:38:19 +0300

    Include User in Extractor interface

--- src/interfaces/extractor.interface.ts
+++ src/interfaces/extractor.interface.ts
@@ -1,5 +1,10 @@
-import { PlatformConnection } from "src/platform-connection/entities/platform-connection.entity";
+import { PlatformConnection } from 'src/platform-connection/entities/platform-connection.entity';
+import { User } from 'src/user/entities/user.entity';
 
 export interface IExtractor {
-    process: (credential:PlatformConnection, {from, until}: {from: Date, until: Date}) => Promise<{processedUntil:Date, processedFrom:Date}>;
+  process: (
+    credential: PlatformConnection,
+    { from, until }: { from: Date; until: Date },
+    user: User,
+  ) => Promise<{ processedUntil: Date; processedFrom: Date }>;
 }
